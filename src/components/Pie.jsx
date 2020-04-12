467bf6ff6febc8b4c214719906495d0f971836a4
Ángel Calderón <angel@acalderon.local>
2020-04-12 18:04:14 -0400

    se asigna variable

--- src/components/Pie.jsx
+++ src/components/Pie.jsx
@@ -9,10 +9,10 @@ export const Pie = ({ data, backgroundColor }) => {
     const dataset = [];
     const assignments = data.map(el => el.assignment).filter((el, i, a) => a.indexOf(el) === i);
     const week = data.filter((el) => el.assignment === select).map(el => el.week).reduce((a, b) => a > b ? a : b, 0);
-    const projects = data.filter((el) => el.assignment === select && el.week === 14).map(el => el.project).filter((el, i, a) => a.indexOf(el) === i);
+    const projects = data.filter((el) => el.assignment === select && el.week === week).map(el => el.project).filter((el, i, a) => a.indexOf(el) === i);
 
     projects.forEach((project, i) => {
-        let hour = data.filter((el) => el.assignment === select && el.week === 14 && el.project === project).map(el => el.hours).reduce((a, b) => a + b, 0);
+        let hour = data.filter((el) => el.assignment === select && el.week === week && el.project === project).map(el => el.hours).reduce((a, b) => a + b, 0);
         dataset.push({ project, hour, backgroundColor: backgroundColor.slice(i, i + 1) });
     });
 
