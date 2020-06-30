54de40b589196580bed8a6d933545947a699b799
Ángel Calderón <angel@acalderon.local>
2020-06-30 17:24:42 -0400

    add moment

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -3,16 +3,20 @@ import useApi from '../../hooks/useApi';
 import { Spinner } from '../UI/others/Spinner';
 import { Card } from '../UI/organisms/Card';
 import { useMemo } from 'react';
+import * as moment from 'moment';
 
 export const Project = () => {
   // const { response, error, isLoading } = useApi('https://demo2961273.mockable.io/api');
-  const { response, error, isLoading } = useApi('/api/team/7?week=24');
+  const weekNumber = moment().week();
+  const { response, error, isLoading } = useApi(
+    `/api/team/7?week=${weekNumber - 1}`
+  );
   const [data, setData] = useState([]);
   useMemo(() => {
     setData(response.data);
   }, [response.data]);
 
-  return <>{isLoading ? <Spinner /> : <Card items={data}></Card>}</>;
+  return <>{isLoading ? <Spinner /> : <Card items={data} />}</>;
 };
 
 export default Project;
